79662cee008b1c38c21e28ec1d5bd4b0a2c3891b
arpithasureshappa <arpitha.sureshappa@tarento.com>
2024-09-02 18:30:24 +0530

    cios upload api updated and download api added

--- src/main/java/org/sunbird/common/util/CbExtServerProperties.java
+++ src/main/java/org/sunbird/common/util/CbExtServerProperties.java
@@ -2716,6 +2716,18 @@ public class CbExtServerProperties {
 		this.insightsMappingKey = insightsMappingKey;
 	}
 
+	public String getCiosCloudContainerName() {
+		return ciosCloudContainerName;
+	}
+
+	public String getCiosCloudFolderName() {
+		return ciosCloudFolderName;
+	}
+
+	public void setCiosCloudContainerName(String ciosCloudContainerName) {
+		this.ciosCloudContainerName = ciosCloudContainerName;
+	}
+
 	public String getSbSearchServiceHost() {
 		return sbSearchServiceHost;
 	}
