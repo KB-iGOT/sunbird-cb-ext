3d1f9e6bb1b422633bbbe9867507be452dd4476d
sreerag-tarento <sreerag.sajeesh@tarento.com>
2024-09-30 22:54:45 +0530

    Resolving conflicts

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -1209,6 +1209,13 @@ public class Constants {
 	public static final String PREVIOUS_TERM_CODE = "previousTermCode";
 	public static final String DESIGNATION_MASTER_DATA = "designation_master_data";
 	public static final String COMPETENCY_MASTER_DATA = "competency_master_data";
+	public static final String UPDATE_AS_NOT_MY_USER = "updatedAsNotMyUserOn";
+	public static final String API_USER_MIGRATION = "api.user.migrated";
+	public static final String PROFILE_DETAILS_PROFILE_STATUS = "profileDetails.profileStatus";
+	public static final String PROFILE_DETAILS_UPDATEDAS_NOT_MY_USER_ON = "profileDetails.updatedAsNotMyUserOn";
+	public static final String NOT_APPLICABLE = "Not Applicable";
+	public static final String CURRENT_PAGE = "currentPage";
+	public static final String FILTER = "filter";
 	public static final String FAILED_TO_UPDATE_PROGRESS = "Failed to update progress of assessment using content state update API";
 
 	private Constants() {
