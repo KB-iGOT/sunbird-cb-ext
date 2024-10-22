33f021b5c3ff209ca658e097bed7ec393c75c253
sreerag-tarento <sreerag.sajeesh@tarento.com>
2024-10-22 17:21:44 +0530

    "Public user event bulk onboard - Bug Fix" (#154)
    * "Public user event bulk onboard - Bug Fix"
    
    * Review changes

--- src/main/java/org/sunbird/nlw/service/CertificateServiceImpl.java
+++ src/main/java/org/sunbird/nlw/service/CertificateServiceImpl.java
@@ -1,13 +1,12 @@
 package org.sunbird.nlw.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.sunbird.common.util.CbExtServerProperties;
-import org.sunbird.core.producer.Producer;
 
 import java.io.IOException;
 import java.util.*;
@@ -19,36 +18,60 @@ public class CertificateServiceImpl {
     CbExtServerProperties serverProperties;
 
     @Autowired
-    Producer kafkaProducer;
-
-    @Autowired
     KafkaTemplate<String, String> kafkaTemplate;
-
-    ObjectMapper objectMapper = new ObjectMapper();
 
     private boolean pushTokafkaEnabled = true;
 
+    ObjectMapper mapper = new ObjectMapper();
+
     public void generateCertificateEventAndPushToKafka(String userId, String eventId, String batchId, double completionPercentage) throws IOException {
-        long now = System.currentTimeMillis();
         List<String> userIds = Collections.singletonList(userId);
-
-        // Create the event JSON string
-        String event = String.format(
-                "{ \"actor\": { \"id\": \"Issue Certificate Generator\", \"type\": \"System\" }, " +
-                        "\"context\": { \"pdata\": { \"version\": \"1.0\", \"id\": \"org.sunbird.learning.platform\" } }, " +
-                        "\"edata\": { \"action\": \"issue-event-certificate\", \"eventType\": \"offline\", \"batchId\": \"%s\", " +
-                        "\"eventId\": \"%s\", \"userIds\": \"%s\", \"eventCompletionPercentage\": %.2f }, " +
-                        "\"eid\": \"BE_JOB_REQUEST\", " +
-                        "\"ets\": %d, " +
-                        "\"mid\": \"EVENT.%s\", " +
-                        "\"object\": { \"id\": \"%s\", \"type\": \"IssueCertificate\" } }",
-                batchId, eventId, userIds.toString(), completionPercentage, now, UUID.randomUUID(), userId
-        ).replaceAll("\n", "");
-
-        // If pushing to Kafka is enabled, send the message
+        String eventJson = generateIssueCertificateEvent(batchId, eventId, userIds, completionPercentage, userId);
         if (pushTokafkaEnabled) {
             String topic = serverProperties.getUserIssueCertificateForEventTopic();
-            kafkaTemplate.send(topic, userId, event);
+            kafkaTemplate.send(topic, userId, eventJson);
         }
+    }
+
+    public String generateIssueCertificateEvent(String batchId, String eventId, List<String> userIds, double eventCompletionPercentage, String userId) throws JsonProcessingException {
+        // Generate current timestamp (in milliseconds)
+        long ets = System.currentTimeMillis();
+
+        // Generate a UUID for the message ID
+        String mid = UUID.randomUUID().toString();
+
+        Map<String, Object> event = new HashMap<>();
+
+        Map<String, Object> actor = new HashMap<>();
+        actor.put("id", "Issue Certificate Generator");
+        actor.put("type", "System");
+        event.put("actor", actor);
+
+        Map<String, Object> context = new HashMap<>();
+        JSONObject pdata = new JSONObject();
+        pdata.put("version", "1.0");
+        pdata.put("id", "org.sunbird.learning.platform");
+        context.put("pdata", pdata);
+        event.put("context", context);
+
+        Map<String, Object> edata = new HashMap<>();
+        edata.put("action", "issue-event-certificate");
+        edata.put("eventType", "offline"); // Add mode here
+        edata.put("batchId", batchId);
+        edata.put("eventId", eventId);
+        edata.put("userIds", userIds);
+        edata.put("eventCompletionPercentage", eventCompletionPercentage);
+        event.put("edata", edata);
+
+        event.put("eid", "BE_JOB_REQUEST");
+        event.put("ets", ets);
+        event.put("mid", mid);
+
+        Map<String, Object> object = new HashMap<>();
+        object.put("id", userId);
+        object.put("type", "IssueCertificate");
+        event.put("object", object);
+
+        return mapper.writeValueAsString(event);
     }
 }
