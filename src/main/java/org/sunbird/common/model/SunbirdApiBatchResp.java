1ff8f486d136654dfc63e52bfe1a35ab3d461bf0
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2025-02-21 18:35:20 +0530

    Revert "4.8.21 multi batch issue (#312)" (#320)
    This reverts commit 6ad39fd44d16ff77d904b9ae8d4390c9caf2b976.

--- src/main/java/org/sunbird/common/model/SunbirdApiBatchResp.java
+++ src/main/java/org/sunbird/common/model/SunbirdApiBatchResp.java
@@ -1,9 +1,7 @@
 package org.sunbird.common.model;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -17,7 +15,6 @@ public class SunbirdApiBatchResp implements Serializable {
 	private String enrollmentEndDate;
 	private String startDate;
 	private int status;
-	private Map<String, Object> batchAttributes = new HashMap<>();
 	public List<String> getCreatedFor() {
 		return createdFor;
 	}
@@ -65,13 +62,5 @@ public class SunbirdApiBatchResp implements Serializable {
 	}
 	public void setStatus(int status) {
 		this.status = status;
-	}
-
-	public Map<String, Object> getBatchAttributes() {
-		return batchAttributes;
-	}
-
-	public void setBatchAttributes(Map<String, Object> batchAttributes) {
-		this.batchAttributes = batchAttributes;
 	}
 }
