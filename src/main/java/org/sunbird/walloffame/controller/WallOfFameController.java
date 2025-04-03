357d61b6ef1c7aabb2321beece9de78223bd2260
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2025-04-03 17:52:14 +0530

    4.8.23 dev v1 (#345)
    * KB-5949 | fix for renaming halloffame to walloffame (#343)
    
    * Added fixes and backward compatability for halloffame APIs
    
    ---------
    
    Co-authored-by: MukeshLingutla <168708131+MukeshLingutla@users.noreply.github.com>

--- src/main/java/org/sunbird/walloffame/controller/WallOfFameController.java
+++ src/main/java/org/sunbird/walloffame/controller/WallOfFameController.java
@@ -1,4 +1,4 @@
-package org.sunbird.halloffame.controller;
+package org.sunbird.walloffame.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.sunbird.common.model.SBApiResponse;
 import org.sunbird.common.util.Constants;
-import org.sunbird.halloffame.service.HallOfFameService;
+import org.sunbird.walloffame.service.WallOfFameService;
 
 import java.util.Map;
 
@@ -14,21 +14,21 @@ import java.util.Map;
  * @author mahesh.vakkund & Deepak kr Thakur
  */
 @RestController
-public class HallOfFameController {
+public class WallOfFameController {
     @Autowired
-    private HallOfFameService hallOfFameService;
+    private WallOfFameService wallOfFameService;
 
-    @PostMapping("/v1/halloffame/read")
-    public ResponseEntity<Map<String, Object>> fetchHallOfFameData() {
-        Map<String, Object> hallOfFameDataMap = hallOfFameService.fetchHallOfFameData();
-        return new ResponseEntity<>(hallOfFameDataMap, HttpStatus.OK);
+    @PostMapping({"/v1/halloffame/read", "/v1/walloffame/read"})
+    public ResponseEntity<Map<String, Object>> fetchWallOfFameData() {
+        Map<String, Object> wallOfFameDataMap = wallOfFameService.fetchWallOfFameData();
+        return new ResponseEntity<>(wallOfFameDataMap, HttpStatus.OK);
     }
 
-    @GetMapping("/v1/halloffame/learnerleaderboard")
+    @GetMapping({"/v1/halloffame/learnerleaderboard", "/v1/walloffame/learnerleaderboard"})
     public ResponseEntity <SBApiResponse> learnerLeaderBoard
             (@RequestHeader(Constants.X_AUTH_TOKEN) String authToken,
              @RequestHeader(Constants.X_AUTH_USER_ORG_ID) String rootOrgId) throws Exception {
-        SBApiResponse response = hallOfFameService.learnerLeaderBoard(rootOrgId, authToken);
+        SBApiResponse response = wallOfFameService.learnerLeaderBoard(rootOrgId, authToken);
         return new ResponseEntity<>(response, response.getResponseCode());
     }
 
@@ -36,25 +36,25 @@ public class HallOfFameController {
     public ResponseEntity<SBApiResponse> fetchingTopLearners
             (@RequestHeader(Constants.X_AUTH_TOKEN) String authToken,
             @PathVariable String ministryOrgId) throws Exception {
-        SBApiResponse response = hallOfFameService.fetchingTop10Learners(ministryOrgId, authToken);
+        SBApiResponse response = wallOfFameService.fetchingTop10Learners(ministryOrgId, authToken);
         return new ResponseEntity<>(response, response.getResponseCode());
     }
 
-    @GetMapping("v1/halloffame/user/read")
+    @GetMapping({"v1/halloffame/user/read", "v1/walloffame/user/read"})
     public ResponseEntity<SBApiResponse> getUserLeaderBoard(@RequestHeader(Constants.X_AUTH_TOKEN) String authToken) {
-        SBApiResponse response = hallOfFameService.getUserLeaderBoard(authToken);
+        SBApiResponse response = wallOfFameService.getUserLeaderBoard(authToken);
         return new ResponseEntity<>(response, response.getResponseCode());
     }
 
-    @GetMapping("v1/halloffame/mdoleaderboard")
+    @GetMapping({"v1/halloffame/mdoleaderboard", "v1/walloffame/mdoleaderboard"})
     public ResponseEntity<SBApiResponse> getMdoLeaderBoard() {
-        SBApiResponse response = hallOfFameService.getMdoLeaderBoard();
+        SBApiResponse response = wallOfFameService.getMdoLeaderBoard();
         return new ResponseEntity<>(response, response.getResponseCode());
     }
 
-    @PostMapping("v1/halloffame/state/mdoleaderboard")
+    @PostMapping({"v1/halloffame/state/mdoleaderboard", "v1/walloffame/state/mdoleaderboard"})
     public ResponseEntity<SBApiResponse> getStateMdoLeaderBoard(@RequestBody Map<String, Object> request) {
-        SBApiResponse response = hallOfFameService.getStateMdoLeaderBoard(request);
+        SBApiResponse response = wallOfFameService.getStateMdoLeaderBoard(request);
         return new ResponseEntity<>(response, response.getResponseCode());
     }
 
@@ -62,7 +62,7 @@ public class HallOfFameController {
     public ResponseEntity<SBApiResponse> fetchingStateTopLearners
             (@RequestHeader(Constants.X_AUTH_TOKEN) String authToken,
              @PathVariable String stateOrgId) throws Exception {
-        SBApiResponse response = hallOfFameService.fetchingStateTop10Learners(stateOrgId, authToken);
+        SBApiResponse response = wallOfFameService.fetchingStateTop10Learners(stateOrgId, authToken);
         return new ResponseEntity<>(response, response.getResponseCode());
     }
 
