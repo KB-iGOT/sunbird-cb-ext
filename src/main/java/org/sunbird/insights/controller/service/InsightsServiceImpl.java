c0d5ac8f331dc8195ffacf8ed2c3bda0d2fa4197
pathiknandi <pathik.nandi@tarento.com>
2025-04-16 11:32:03 +0530

    solveing conflict error and build error

--- src/main/java/org/sunbird/insights/controller/service/InsightsServiceImpl.java
+++ src/main/java/org/sunbird/insights/controller/service/InsightsServiceImpl.java
@@ -355,7 +355,6 @@ public class InsightsServiceImpl implements InsightsService {
                 } else {
                     redisData = redisCacheMgr.getHashedCacheFromDataRedis(redisKey, serverProperties.getRedisInsightIndex(), orgId);
                 }
-                String redisData = redisCacheMgr.getCacheFromDataRedish(redisKey, serverProperties.getRedisInsightIndex());
                 Map<String, Object> insightData = new HashMap<>();
                 String iconUrl = fieldIcons.get(insightLabel);
                 insightData.put(ICON, iconUrl);
