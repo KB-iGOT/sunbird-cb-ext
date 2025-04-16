a42c8774ba83431db414c357a4861d511190d0c0
pathiknandi <pathik.nandi@tarento.com>
2025-04-16 10:06:10 +0530

    new changes for redis method instance of hget now using get, And no need orgId.

--- src/main/java/org/sunbird/insights/controller/service/InsightsServiceImpl.java
+++ src/main/java/org/sunbird/insights/controller/service/InsightsServiceImpl.java
@@ -349,7 +349,12 @@ public class InsightsServiceImpl implements InsightsService {
             for (Map.Entry<String, String> entry : redisKeyMapping.entrySet()) {
                 String insightLabel = entry.getKey();
                 String redisKey = entry.getValue();
-                String redisData = redisCacheMgr.getCacheFromDataRedish(redisKey, serverProperties.getRedisInsightIndex());
+                String redisData;
+                if (Constants.EVENTS_PUBLISHED.equalsIgnoreCase(insightLabel)) {
+                    redisData = redisCacheMgr.getCacheFromDataRedish(redisKey, serverProperties.getRedisInsightIndex());
+                } else {
+                    redisData = redisCacheMgr.getHashedCacheFromDataRedis(redisKey, serverProperties.getRedisInsightIndex(), orgId);
+                }
                 Map<String, Object> insightData = new HashMap<>();
                 String iconUrl = fieldIcons.get(insightLabel);
                 insightData.put(ICON, iconUrl);
