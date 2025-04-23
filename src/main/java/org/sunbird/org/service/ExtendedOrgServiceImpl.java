51989950ccab0b7614b8799d0cc4daaff1a1a77a
ajay-vishwkarma <ajay.vishwkarma@tarento.com>
2025-04-23 16:46:54 +0530

    KB-9344 | use constant for FAILED

--- src/main/java/org/sunbird/org/service/ExtendedOrgServiceImpl.java
+++ src/main/java/org/sunbird/org/service/ExtendedOrgServiceImpl.java
@@ -1114,7 +1114,7 @@ public class ExtendedOrgServiceImpl implements ExtendedOrgService {
 		for (String fieldName : request.keySet()) {
 			if (!validateUpdatableFields(fieldName)) {
 				logger.info("ExtendedOrgServiceImpl::updateV2::Invalid field in request: {}", fieldName);
-				response.getParams().setStatus("Failed");
+				response.getParams().setStatus(Constants.FAILED);
 				response.setResponseCode(HttpStatus.BAD_REQUEST);
 				return "Field : " + fieldName + " is not allowed to be updated.";
 			} else {
