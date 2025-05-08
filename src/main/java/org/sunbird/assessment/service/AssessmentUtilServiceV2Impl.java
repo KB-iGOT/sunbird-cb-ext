f048a49e1c6dff821b227cae31301b84b7e963c1
karthik-tarento <karthikeyan.rajendran@tarento.com>
2025-05-08 22:12:42 +0530

    Fixed build issues

--- src/main/java/org/sunbird/assessment/service/AssessmentUtilServiceV2Impl.java
+++ src/main/java/org/sunbird/assessment/service/AssessmentUtilServiceV2Impl.java
@@ -1114,11 +1114,9 @@ public class AssessmentUtilServiceV2Impl implements AssessmentUtilServiceV2 {
 						errMsg = "API doesn’t support this feature";
 						ProjectUtil.updateErrorDetails(response, errMsg, HttpStatus.INTERNAL_SERVER_ERROR);
 						return errMsg;
-					} else {
-						logger.info("AssessmentUtilServiceV2Impl:: validateContextLocking:: user has completed all the children courses");
 					}
 				} else {
-					errMsg = "content Details not found from cache";
+					errMsg = "Content Details not found from cache";
 					ProjectUtil.updateErrorDetails(response, errMsg, HttpStatus.INTERNAL_SERVER_ERROR);
 					return errMsg;
 				}
