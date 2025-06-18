e22bf0e701ed03965a52804092b00406da6d5a50
anilkumarkammalapalli <anilkumar.kammalapalli@tarento.com>
2025-06-18 16:56:03 +0530

    update redis key

--- src/main/java/org/sunbird/org/util/FrameworkUtil.java
+++ src/main/java/org/sunbird/org/util/FrameworkUtil.java
@@ -95,11 +95,11 @@ public class FrameworkUtil {
     }
 
     private List<Map<String, Object>> getMasterData(String orgId) throws Exception, InterruptedException {
-        String masterDataOrg = redisCacheMgr.getCache(Constants.ORG_MASTER_DATA);
+        String masterDataOrg = redisCacheMgr.getCache(Constants.ORG_MASTER_DATA+"_"+orgId);
         if (StringUtils.isEmpty(masterDataOrg) || masterDataOrg.equals("[]") || masterDataOrg.equals("{}") || masterDataOrg.equalsIgnoreCase("null")) {
             List<Map<String, Object>> orgMasterData = populateDataFromApi(orgId);
             if (orgMasterData != null) {
-                redisCacheMgr.putCache(Constants.ORG_MASTER_DATA, orgMasterData, serverProperties.getRedisMasterDataReadTimeOut());
+                redisCacheMgr.putCache(Constants.ORG_MASTER_DATA+"_"+orgId, orgMasterData, serverProperties.getRedisMasterDataReadTimeOut());
                 return orgMasterData;
             } else {
                 return Collections.emptyList();
