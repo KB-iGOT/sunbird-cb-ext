7352a2dcd22266f12d7cd40c97bbf4c6bdcf1cc1
Pathik Nandi <86921116+Pathik-Nandi@users.noreply.github.com>
2025-07-16 13:33:37 +0530

    adding logs for language in autoEnrollment API. (#462)
    * to avoid the null point exception i have added condition on batch.
    
    * adding logs for language in autoEnrollment API.
    
    * adding logs for language in autoEnrollment API.
    
    ---------
    
    Co-authored-by: pathiknandi <pathik.nandi@tarento.com>

--- src/main/java/org/sunbird/assessment/service/CohortsServiceImpl.java
+++ src/main/java/org/sunbird/assessment/service/CohortsServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,6 +35,7 @@ import org.sunbird.user.service.UserUtilityService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 
+@Slf4j
 @Service
 public class CohortsServiceImpl implements CohortsService {
 
@@ -178,6 +180,7 @@ public class CohortsServiceImpl implements CohortsService {
 		Map<String, Object> enrollMentResponse = outboundRequestHandlerService.fetchResultUsingPost(
 				cbExtServerProperties.getCourseServiceHost() + cbExtServerProperties.getUserCourseEnroll(), req,
 				headers);
+		log.info("Response received from enroll POST{}",enrollMentResponse);
 		return enrollMentResponse;
 	}
 
@@ -377,6 +380,7 @@ public class CohortsServiceImpl implements CohortsService {
 				return errResponse;
 			}
 			//Enroll for the 1st batch for the course, Standalone Assessment
+			logger.info("Enrolling user with contentId: " + contentId + ", language: " + language);
 			Map<String, Object> enrollResponse = enrollInCourse(contentId, userUUID, headers, batchDetail.getBatchId(), language);
 			if (!ObjectUtils.isEmpty(enrollResponse) && Constants.OK.equals(enrollResponse.get(Constants.RESPONSE_CODE))) {
 				finalResponse = constructAutoEnrollResponse(batchDetail);
