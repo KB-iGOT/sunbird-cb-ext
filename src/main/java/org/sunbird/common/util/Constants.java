c8ca85bc9c098b4f60eb091344a162624474a198
pathiknandi <pathik.nandi@tarento.com>
2025-07-29 15:57:01 +0530

    Bug is fixed for publish API where isApar was mismatching.

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -1389,7 +1389,6 @@ public class Constants {
 	public static final String LANGUAGE = "language";
 	public static final String TABLE_USER_ENROLMENT_V2 = "user_enrolments_v2";
 	public static final String IS_APAR ="isApar";
-	public static final String IS_APAR_KEY ="isapar";
 	private Constants() {
 		throw new IllegalStateException("Utility class");
 	}
