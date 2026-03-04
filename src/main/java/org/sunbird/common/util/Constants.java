c321b958230d9b9338c94831e92e45e035f80b6c
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2026-03-04 10:24:00 +0530

    Cbrelease 4.8.34 vapt (#580)
    * KB-13294,KB-13360 : updated file updation for vapt
    
    * config update
    
    * updated the changes
    
    * KB-11881: added a verify otp method to velidate users phone and email update (#548)
    
    * formatted the changes
    
    * vapt : KB-13305 updated missing logic validate using MagicBytes
    
    * optimisation :: update usertoken validation : avoiding multiple validations
    
    * vapt fix for KB-13432 :  CWE-20 : Character Limit Bypass

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -1479,4 +1479,5 @@ public class Constants {
     public static final String EMAIL_OTP = "emailOtp";
     public static final String PHONE_OTP = "phoneOtp";
 	public static final String JPEG = "jpeg";
+	public static final String ABOUT_ME = "aboutme";
 }
