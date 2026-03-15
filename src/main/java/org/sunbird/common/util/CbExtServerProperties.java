ca59bb323777eda0d6771d7c76f2006bf051a7a4
anilkumarkammalapalli <121931293+anilkumarkammalapalli@users.noreply.github.com>
2026-03-15 08:02:03 +0530

    KB-13537 added api to upload mp4 and pdf files at the time of survey … (#594)
    * KB-13537 added api to upload mp4 and pdf files at the time of survey submit
    
    * KB-13537 update folder name for peer validation survey submissions
    
    * Update application.properties
    
    * KB-13537 retrieving userid from token

--- src/main/java/org/sunbird/common/util/CbExtServerProperties.java
+++ src/main/java/org/sunbird/common/util/CbExtServerProperties.java
@@ -1189,6 +1189,15 @@ public class CbExtServerProperties {
 	@Value("${peervalidation.cloud.folder.name}")
 	private String peerValidationCloudFolderName;
 
+	@Value("${peer.validation.pdf.max.size}")
+	private long peerValidationPdfMaxSize;
+
+	@Value("${peer.validation.video.max.size}")
+	private long peerValidationVideoMaxSize;
+
+	@Value("${peer.validation.submission.folder}")
+	private String peerValidationSubmissionFolder;
+
 	public String getStateLearningInsightsRedisKeyMapping() {
 		return stateLearningInsightsRedisKeyMapping;
 	}
@@ -3994,4 +4003,16 @@ public class CbExtServerProperties {
 	public String getPeerValidationCloudFolderName() { return peerValidationCloudFolderName; }
 
 	public void setPeerValidationCloudFolderName(String peerValidationCloudFolderName) { this.peerValidationCloudFolderName = peerValidationCloudFolderName; }
+
+	public long getPeerValidationPdfMaxSize() {
+		return peerValidationPdfMaxSize;
+	}
+
+	public long getPeerValidationVideoMaxSize() {
+		return peerValidationVideoMaxSize;
+	}
+
+	public String getPeerValidationSubmissionFolder() {
+		return peerValidationSubmissionFolder;
+	}
 }
