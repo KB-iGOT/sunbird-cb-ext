425656e587af89ccecc469cf43dc5dc6200831ef
adarsh-tarento <adarsh.malavade@tarento.com>
2026-03-30 19:14:44 +0530

    KB-13560: fix admin user patch API (500 error) to if we add profile Preference field (#616)

--- src/main/java/org/sunbird/profile/service/ProfileServiceImpl.java
+++ src/main/java/org/sunbird/profile/service/ProfileServiceImpl.java
@@ -2120,6 +2120,11 @@ public class ProfileServiceImpl implements ProfileService {
 						}
 					} else if (profileDetailsMap.get(changedObj) instanceof Boolean) {
 						existingProfileDetails.put(changedObj, profileDetailsMap.get(changedObj));
+					} else if (profileDetailsMap.get(changedObj) instanceof Number) {
+						if (!handleProfilePreferenceUpdate(changedObj, profileDetailsMap.get(changedObj),
+								existingProfileDetails, response)) {
+							return response;
+						}
 					} else {
 						if (existingProfileDetails.containsKey(changedObj)) {
 							Map<String, Object> existingProfileChild = (Map<String, Object>) existingProfileDetails
@@ -2271,6 +2276,41 @@ public class ProfileServiceImpl implements ProfileService {
 		}
 		return response;
 
+	}
+
+	private boolean handleProfilePreferenceUpdate(String changedObj, Object rawValue,
+	                                              Map<String, Object> existingProfileDetails, SBApiResponse response) {
+
+		if (!Constants.PROFILE_PREFERENCE.equalsIgnoreCase(changedObj)) {
+			response.setResponseCode(HttpStatus.BAD_REQUEST);
+			response.getParams().setStatus(Constants.FAILED);
+			response.getParams().setErrmsg(Constants.INVALID_PROFILE_DETAILS_FIELD + changedObj);
+			return false;
+		}
+
+		Integer profilePreference = (Integer) rawValue;
+
+		boolean isAllowed = false;
+		if (serverProperties.getProfilePreferenceValue() != null) {
+			String pref = String.valueOf(profilePreference);
+			String[] allowedValues = serverProperties.getProfilePreferenceValue().split(",", -1);
+			for (String allowed : allowedValues) {
+				if (pref.equals(StringUtils.trimToEmpty(allowed))) {
+					isAllowed = true;
+					break;
+				}
+			}
+		}
+
+		if (isAllowed) {
+			existingProfileDetails.put(changedObj, profilePreference);
+			return true;
+		}
+
+		response.setResponseCode(HttpStatus.BAD_REQUEST);
+		response.getParams().setStatus(Constants.FAILED);
+		response.getParams().setErrmsg(Constants.INVALID_PROFILE_PREFERENCE_VALUE + serverProperties.getProfilePreferenceValue());
+		return false;
 	}
 
 	@Override
