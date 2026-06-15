ebb3fbeb7c5359f8eef8fc7ba240ffed20997f71
ramyaranganathan-1775 <ramya.ranganathan@tarento.com>
2026-06-15 16:31:12 +0530

    KB-14330- Profile Page wrapper Api Implementation for Volunteer users

--- src/main/java/org/sunbird/common/service/OutboundRequestHandlerServiceImpl.java
+++ src/main/java/org/sunbird/common/service/OutboundRequestHandlerServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.Https;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
