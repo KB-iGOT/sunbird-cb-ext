306425eff3fd4619f4048aa69cb2583e6adb38ec
ramyaranganathan-1775 <ramya.ranganathan@tarento.com>
2026-06-30 11:40:48 +0530

    test logs

--- src/main/java/org/sunbird/bpreports/service/BPReportsServiceV2Impl.java
+++ src/main/java/org/sunbird/bpreports/service/BPReportsServiceV2Impl.java
@@ -403,6 +403,7 @@ public class BPReportsServiceV2Impl implements BPReportsServiceV2 {
         logger.debug("BPReportsServiceV2Impl:: processBPReportV2: Batch details fetched for batchId: {}", batchId);
 
         List<WfStatusEntity> enrolledUsers = fetchEnrolledUsers(batchId);
+        logger.info("BPReportsServiceV2Impl:: test log enrolledUsers {}", enrolledUsers.toString());
         if (enrolledUsers.isEmpty()) {
             logger.warn("BPReportsServiceV2Impl:: processBPReportV2: No active enrolled users found for batchId: {}", batchId);
         }
@@ -418,6 +419,7 @@ public class BPReportsServiceV2Impl implements BPReportsServiceV2 {
         Map<String, Object> allUserProfiles = batchFetchUserProfiles(userIds);
         Map<String, List<Map<String, Object>>> allCertStatuses = batchFetchCertificateStatuses(userIds, courseId, batchId);
         logger.debug("BPReportsServiceV2Impl:: processBPReportV2: User profiles and certificate statuses fetched for batchId: {}", batchId);
+        logger.debug("BPReportsServiceV2Impl:: processBPReportV2: User profiles and certificate allCertStatuses: {}", allCertStatuses);
 
         List<String> createdFor = (List<String>) batchDetails.get(Constants.CREATED_FOR);
         String contentOrgId = CollectionUtils.isEmpty(createdFor) ? null : createdFor.get(0);
@@ -1095,7 +1097,7 @@ public class BPReportsServiceV2Impl implements BPReportsServiceV2 {
         LocalDate start = parseToLocalDate(batchDetails.get(Constants.START_DATE_COLUMN));
         LocalDate end = parseToLocalDate(batchDetails.get(Constants.END_DATE_COLUMN));
         if (Objects.nonNull(start) && Objects.nonNull(end)) {
-            return String.valueOf(ChronoUnit.DAYS.between(start, end));
+            return String.valueOf(ChronoUnit.DAYS.between(start, end))+1;
         }
         return "";
     }
